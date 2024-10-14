9204fc272b27c5195977dab00d554bd112612a7d
Carl Gwilliam <gwilliam@hep.ph.liv.ac.uk>
2024-10-14 12:37:34 +0100

    Update notebooks and instructions

--- src/HtautauRegression/helpers.py
+++ src/HtautauRegression/helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import vector
 
 def find_sample_number(samples, mass):
     idx = [mass in d for d in samples].index(True)
@@ -10,7 +11,8 @@ def find_indices(samples, aux, aux_labels, mass):
     idx = find_sample_number(samples, str(mass))
     return [(a==idx)[isample] for a in aux]
 
-def plot_mass(m = None, mmmc = None, msv = None, mtrue = None, bins = np.linspace(50, 200, 100), 
+def plot_mass(m = None, mmmc = None, msv = None, mtrue = None, mvis = None,
+              bins = np.linspace(50, 200, 100), 
               title = "", true_scale = 1, logy = False):
 
     plt.figure()
@@ -49,23 +51,56 @@ def plot_mass(m = None, mmmc = None, msv = None, mtrue = None, bins = np.linspac
         
     if mtrue is not None:
         
-        # if scale_true:
-        #     counts, _ = np.histogram(mtrue, bins = len(bins), range = (bins[0], bins[-1]))
-        #     counts = counts/float(sum(counts))
-        #     scale = ymax/max(counts)
-        #     mtrue_scaled = mtrue * scale
-        
-        
         counts, bins2 = np.histogram(mtrue, bins = len(bins), range = (bins[0], bins[-1]))
         counts = counts/(sum(counts) * (bins[1] - bins[0]))
         counts = counts * true_scale
         y, x, polys = plt.hist(bins2[:-1], bins, weights=counts, fill = None, histtype = "step",
                                label = "True" + (rf" $\times$ {true_scale}" if true_scale != 1 else ""))    
-        #scale2 = ymax/max(y)
-        #print(scale2)
-        #polys[0].set_xy([[x,y*scale] for x, y in polys[0].get_xy()])
 
+    if mvis is not None:
+        mean_vis = np.mean(mvis)
+        std_vis = np.std(mvis)
+        y, x, _ = plt.hist(mvis, bins = bins, fill = None, histtype = "step", density = True, 
+                           label = f"Vis ($\mu$ = {mean_vis:.1f}, $\sigma$ = {std_vis:.1f})")  
+        ymax = max(y) if max(y) > ymax else ymax
+    
     plt.xlabel("M [GeV] ")
     plt.ylabel("A.U")
     plt.legend()
     plt.show()
+
+def make_four_vector(varname, data, massidx = None):
+
+    # Find variable indices 
+    ipt = data.feature_names.index(f"{varname}_Pt")
+    ieta = data.feature_names.index(f"{varname}_Eta")
+    iphi = data.feature_names.index(f"{varname}_Phi")
+    try:
+        nme = "M"
+        ime = data.feature_names.index(f"{varname}_M")
+    except ValueError:
+        nme = "E"
+        ime = data.feature_names.index(f"{varname}_E")        
+
+    # Get input features
+    X = data.X()
+    
+    if massidx is None:
+        vec = vector.array(
+            {"pt"  : X[:, ipt], 
+             "eta" : X[:, ieta], 
+             "phi" : X[:, iphi], 
+             nme   : X[:, ime]
+            }
+        )      
+    else:
+        vec = vector.array(
+            {"pt"  : X[massidx, ipt], 
+             "eta" : X[massidx, ieta], 
+             "phi" : X[massidx, iphi], 
+             nme   : X[massidx, ime]
+            }
+        )
+    
+    return vec
+
